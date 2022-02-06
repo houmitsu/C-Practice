4c2c324e728c842f0975a10a11d90f4364e8631c
Fumiya Houmitsu <fumiya.houmitsu1028@gmail.com>
2022-02-06 23:41:02 +0900

    変数を作る

--- Assets/Scripts/LearnigCurve.cs
+++ Assets/Scripts/LearnigCurve.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class LearnigCurve : MonoBehaviour
 {
+    public int currentAge = 30;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(30 + 1);
+
+        Debug.Log(currentAge + 1);
     }
 
     // Update is called once per frame

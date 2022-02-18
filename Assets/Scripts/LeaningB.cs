a617daa0a8a285fc85265e8c757ef98bfb006b90
Fumiya Houmitsu <fumiya.houmitsu1028@gmail.com>
2022-02-19 00:13:36 +0900

    Dictionary Update

--- Assets/Scripts/LeaningB.cs
+++ Assets/Scripts/LeaningB.cs
@@ -7,14 +7,28 @@ public class LeaningB : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         Dictionary<string, int> itemInventory = new Dictionary<string, int>()
         {
             {"Potion", 5},
-            {"Antidote", 8 },
-            {"Aspirin", 1 }
+            {"Antidote", 8},
+            {"Aspirin", 1}
         };
 
-        Debug.LogFormat("Item: {0}", itemInventory.Count);
+
+        //int numbberOfPotions = itemInventory["Potion"];
+        itemInventory["Potion"] = 10;
+        itemInventory.Add("Throwing Knife", 3);
+        itemInventory["Bandage"] = 5;
+
+        if(itemInventory.ContainsKey("Aspirin"))
+        {
+            itemInventory["Aspirin"] = 3;
+        }
+
+        itemInventory.Remove("Antidote");
+
+        Debug.LogFormat("Item: {0}", itemInventory.Count);  
     }
 
     // Update is called once per frame
